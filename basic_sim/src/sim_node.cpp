a44ccd9ee6e93bf84ad460dfc6758d4cad34499d
Reuben Aronson <rmaronson@cmu.edu>
2022-08-22 19:19:23 -0400

    rework launch files + bugfixes

--- basic_sim/src/sim_node.cpp
+++ basic_sim/src/sim_node.cpp
@@ -44,8 +44,7 @@
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "sim_hw_interface");
-  ros::NodeHandle nh;
-  ros::NodeHandle pnh("~");
+  ros::NodeHandle nh("~");
 
   // NOTE: We run the ROS loop in a separate thread as external calls such
   // as service callbacks to load controllers can block the (main) control loop
@@ -53,7 +52,7 @@ int main(int argc, char** argv)
   spinner.start();
 
   // Create the hardware interface specific to your robot
-  ros_control_boilerplate::SimHWInterface sim_hw_interface(pnh);
+  ros_control_boilerplate::SimHWInterface sim_hw_interface(nh);
   sim_hw_interface.init();
   if (!ros::ok()) {
     return 1;
