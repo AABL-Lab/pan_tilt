2c153755cb88d5d97c3c0400f9fd71af633d4040
aabl daemon <aabl-dev@elist.tufts.edu>
2022-12-02 16:55:21 -0500

    Made pan_tilt_client its own ros python package so its importable (probably overkill)

--- pan_tilt_client/src/pan_tilt_client/pantilt.py
+++ pan_tilt_client/src/pan_tilt_client/pantilt.py
@@ -1,11 +1,11 @@
 
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 
-HARDCODED_PAN_LIMITS = (-0.5, 0.5) # these are just made up
+HARDCODED_PAN_LIMITS = (-0.7, 0.7) # these are just made up
 HARDCODED_TILT_LIMITS = (-0.7, 0.1) # These are empirical but real
 
 class PanTilt():
@@ -34,23 +34,42 @@ class PanTilt():
 
     def pan_go(self, pan_target):
         if pan_target < HARDCODED_PAN_LIMITS[0] or HARDCODED_PAN_LIMITS[1] < pan_target:
-            rospy.logerr(f"Invalid Target {pan_target:1.5f} but limited to {HARDCODED_pan_LIMITS}")
+            rospy.logerr(f"Invalid Target {pan_target:1.5f} but limited to {HARDCODED_PAN_LIMITS}")
             return
         self.pan_pub.publish(pan_target)
+
+    def increment_pantilt(self, pan_inc, tilt_inc):
+        new_pan = self.pan_theta + pan_inc
+        new_tilt = self.tilt_theta + tilt_inc
+        print(f"Incrementing to {new_pan:1.2f} {new_tilt:1.2f}")
+        self.pan_go(new_pan)
+        self.tilt_go(new_tilt)
+
 
 
 if __name__ == "__main__":
     rospy.init_node("PanTilt_Client", anonymous=True)
     pt = PanTilt()
+    tilt0 = 0.0
+    tilt1 = -0.3
+    pan0 = 0.2
+    pan1 = -0.2
     print("Spinning pantilt")
     rospy.sleep(1.0)
-    pt.tilt_go(0.0)
+    print("Tilt to ", tilt0)
+    pt.tilt_go(tilt0)
     rospy.sleep(2.0)
-    pt.tilt_go(-0.3)
+
+    print("Tilt to ", tilt1)
+    pt.tilt_go(tilt1)
     rospy.sleep(2.0)
-    pt.pan_go(0.2)
+
+    print("Pan to ", pan0)
+    pt.pan_go(pan0)
     rospy.sleep(2.0)
-    pt.pan_go(-0.2)
+
+    print("Pan to ", pan1)
+    pt.pan_go(pan1)
 
     rospy.spin()
 
